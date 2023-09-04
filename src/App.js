579b11a0821fa28935b9f42a9d040d64098bd4bd
Mohamed Ashif Ali Jamal Mohamed <55224952+ashif1989@users.noreply.github.com>
2023-09-04 12:28:44 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
+
+  let handleClick = (value) => {
+    if (value === "=") {
+      let output = eval(input);
+      setResult(output);
+    } else if (value === "C") {
+      setInput("");
+      setResult("");
+    } else {
+      setInput((input) => input + value);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Calculator</h1>
+      <input type="text" value={input} readOnly />
+      <div>{result}</div>
+      <div className="row">
+        <button onClick={() => handleClick("7")}>7</button>
+        <button onClick={() => handleClick("8")}>8</button>
+        <button onClick={() => handleClick("9")}>9</button>
+        <button onClick={() => handleClick("/")}>/</button>
+      </div>
+
+      <div className="row">
+        <button onClick={() => handleClick("4")}>4</button>
+        <button onClick={() => handleClick("5")}>5</button>
+        <button onClick={() => handleClick("6")}>6</button>
+        <button onClick={() => handleClick("*")}>X</button>
+      </div>
+
+      <div className="row">
+        <button onClick={() => handleClick("1")}>1</button>
+        <button onClick={() => handleClick("2")}>2</button>
+        <button onClick={() => handleClick("3")}>3</button>
+        <button onClick={() => handleClick("-")}>-</button>
+      </div>
+
+      <div className="row">
+        <button onClick={() => handleClick("C")}>C</button>
+        <button onClick={() => handleClick("=")}>=</button>
+        <button onClick={() => handleClick("%")}>%</button>
+        <button onClick={() => handleClick("+")}>+</button>
+      </div>
+    </div>
+  );
+}
